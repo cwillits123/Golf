269e41d07ab4c677fe03775547e9b366cde3bbbf
Colin Willits <willitscb@willits-mba.local>
2024-05-26 11:34:58 -0400

    Updated Putter

--- src/main/java/Clubs/Putter.java
+++ src/main/java/Clubs/Putter.java
@@ -9,11 +9,11 @@ public class Putter extends GolfClub{
 
   public void swing(Hole h) {
     int yards = h.getDistance();
-    if (Math.abs(yards) < 6){
+    if (Math.abs(yards) < 9){
       h.addStroke();
       Ball.placeBall(h.getYards(), 50);
     }
-    else if (Math.abs(yards) < 14){
+    else if (Math.abs(yards) < 18){
       h.addStroke();
       h.addStroke();
       Ball.placeBall(h.getYards(), 50);
